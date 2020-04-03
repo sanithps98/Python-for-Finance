e380016a67eda34fb54ae4ce005a8ce55008ba0b
Sanith PS <pssanith@karunya.edu.in>
2020-04-03 14:44:28 +0530

    Update Finding_HighDividendStocks.py

--- Finding_HighDividendStocks.py
+++ Finding_HighDividendStocks.py
@@ -1,24 +1,23 @@
+import pandas as pd
 import requests
 import json
 
-demo = 'TYPE YOUR API KEY'
-#tickers = requests.get(f'https://fmpcloud.io/api/v3/symbol/available-nasdaq?apikey={demo}')
+my_apikey = 'TYPE YOUR API KEY'
+tickers = requests.get(f'https://fmpcloud.io/api/v3/symbol/available-nasdaq?apikey={my_apikey}')
+tickers = tickers.json()
 
-tickers = open('tickers.json')
-tickers = json.load(tickers)
+#tickers = open('tickers.json')
+#tickers = json.load(tickers)
 
 symbols = []
 for ticker in tickers:
     symbols.append(ticker['symbol'])
-    
-symbols_short = symbols[0:5]
-print(symbols_short)
   
 DivYield = {}
 
 for company in symbols_short:
   try:
-    companydata = requests.get(f'https://fmpcloud.io/api/v3/profile/{company}?apikey={demo}')
+    companydata = requests.get(f'https://fmpcloud.io/api/v3/profile/{company}?apikey={my_apikey}')
     companydata = companydata.json()
     latest_Annual_Dividend = companydata[0]['lastDiv']
     price = companydata[0]['price']
@@ -39,9 +38,7 @@ for company in symbols_short:
 
 print(DivYield)
 
-import pandas as pd
-
 DivYield_dataframe = pd.DataFrame.from_dict(DivYield, orient='index')
-
 DivYield_dataframe = DivYield_dataframe.sort_values(['Dividend_Yield'], ascending=[False])
+
 print(DivYield_dataframe)
